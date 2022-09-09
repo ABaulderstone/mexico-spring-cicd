d55fc287a50fa9fa9db8f11933c5e107ea135e60
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-09-09 11:19:25 +1000

    basic repo

---
+++ src/test/java/io/nology/mexicospringcicd/MathControllerTest.java
@@ -0,0 +1,32 @@
+package io.nology.mexicospringcicd;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MathControllerTest {
+
+	@Autowired
+	private MathController controller;
+
+	@Test
+	public void rectangleAreaReturnsCorrectArea() {
+		Integer height = 100;
+		Integer width = 200;
+		Integer result = this.controller.rectangleArea(height, width);
+		Integer expected = 20000;
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void circlePerimeterReturnsCorrectResult() {
+		Double radius = 10.00;
+		Double expected = 62.83185307179586;
+    Double actual = this.controller.circlePerimter(radius);
+		assertEquals(expected, actual);
+	}
+
+}
